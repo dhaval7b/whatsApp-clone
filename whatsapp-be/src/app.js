303859cf54397c1f29e5b001775e2ee16bf576b3
dhavalrbhimani <logan7bhimani@gmail.com>
2024-06-09 18:25:05 -0400

    initial fe commit

--- whatsapp-be/src/app.js
+++ whatsapp-be/src/app.js
@@ -1,6 +1,14 @@
 import express from 'express';
-
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+dotenv.config();
 const app = express();
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+// Helmet
+app.use(helmet());
 app.get('/', (req, res) => {
     res.send('Hello from WhatsApp Backend!');
 });
--- whatsapp-be/src/index.js
+++ whatsapp-be/src/index.js
@@ -3,6 +3,7 @@ import app from './app.js';
 
 dotenv.config();
 const PORT = process.env.PORT || 8000;
+console.log(process.env.Node_ENV);
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
